3ab73aa49f93a55f5daf161e19ac74f3388bbc45
tgggwhqdqggqr3gw <91467878+tgggwhqdqggqr3gw@users.noreply.github.com>
2022-08-21 21:53:10 +0530

    Add files via upload

---
+++ q1.java
@@ -0,0 +1,12 @@
+public class q1{
+    public void show( int n ){
+        int i = 0;
+        int j = 0;
+        for(i = 1;i <= n;i++){
+            System.out.println();
+            for(j = 1;j <= i; j++){
+                System.out.print("* ");
+            }
+        }
+    }
+}
